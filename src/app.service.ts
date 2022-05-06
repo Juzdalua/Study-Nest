40c8d039b6156b1413edb00591ba8fbf45c8e03d
Juzdalua <bearwcw@naver.com>
2022-05-06 10:38:53 +0900

    Use mockup-data

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,53 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { ConnectionService } from './connection/connection.service';
 import SQL from 'sql-template-strings';
+import egg from 'src/mockup/egg.json';
 
 @Injectable()
 export class AppService {
-  constructor(
-    private connectionService: ConnectionService
-    ){}
+  constructor(private connectionService: ConnectionService) {}
   async getHello() {
-
-    // const arr = [1,2,3,4,5];
-    // const num = 3;
-    // const a = arr[0];
-
-    // arr.forEach(e => {
-    //   if(e === num){
-    //     console.log("break; ",e)
-    //     return false;
-    //   } else{
-    //     console.log("oh~ ",e)
-    //   }
-    // })
-
-    // for(let i=0; i<arr.length; i++){
-    //   if(arr[i] === num){
-    //     console.log(i, arr[i]);
-    //     return {
-    //       i: arr[i]
-    //     }
-    //   }
-    // }
-
-    // const a = this.connectionService.CP.query("SELECT * FROM USERS", [], (err, results, fields) => {
-    //   if(err)
-    //     throw err;
-    //   return results
-    // }).then(response => console.log(response));
+    const data = egg;
+    let filter = {
+      erc721: [],
+      erc1155: [],
+      contractsInfo: [],
+    };
 
 
-    const a = await this.connectionService.SQL("SELECT * FROM USERS WHERE id = ? LIMIT 1;", [1]);
-    const b = await this.connectionService.SQL(SQL`SELECT * FROM USERS WHERE id = ${1} LIMIT 1;`);
-    const c = await this.connectionService.SQL(SQL`SELECT * FROM USERS WHERE id = ? LIMIT 1;`, [1]);
 
-    return c;
+    const newMap = data.contractsInfo.map(e => {
+      const salesList = [];
+      const yami = data[e.schema.toLowerCase()].forEach((ele:any) => {
+        if(ele.address.toLowerCase() === e.address.toLowerCase()){
+          salesList.push(ele);
+        }
+      })
+      console.log(salesList)
+
+      return {
+        ...e,
+        salesList
+      }
+    });
+
+    newMap.forEach(e => {
+      e.salesList.forEach(ele => {
+        console.log(`🚀 Event:Mint => ${e.name} #${ele.tokenId} / Price: ${data.price}, Gas: ${data.gas} / ${e.address}`);
+      })
+    })
+    return newMap
 
 
-    const [user, field] = await this.connectionService.POOL.query("SELECT * FROM USERS WHERE name = ? ORDER BY id DESC LIMIT 1;", ['jun']);
-    return user;
-  };
-
-
+  }
 }
